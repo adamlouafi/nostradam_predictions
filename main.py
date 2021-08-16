1bb26f830b83f99551bd77105cb9802fdacdd525
Adam Louafi <61043077+adamlouafi@users.noreply.github.com>
2021-08-16 13:06:57 +0200

    commented test function Recommented `selectFixtures()`

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ def sendPicks(date, tg_api_key, chat_id):
 
 def main():
     # settleFixtures(soccer_id, settled_fixtures_url, ps3838_api_key)
-    selectFixtures(soccer_id, odds_url, fixtures_url, today_date, ps3838_api_key)
+    # selectFixtures(soccer_id, odds_url, fixtures_url, today_date, ps3838_api_key)
     # sendPicks(today_date, tg_api_key, tg_chat_id)
     # updateOdds(soccer_id, odds_url, ps3838_api_key)
 
